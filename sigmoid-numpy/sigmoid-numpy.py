2e5a157c10f66cf456a2af7ef3f16933512a0dfc
MRITYUNJAY SINGH <codewithmj890@gmail.com>
2026-03-09 17:37:28 +0530

    Add solution for sigmoid-numpy

---
+++ sigmoid-numpy/sigmoid-numpy.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def sigmoid(x):
+    x = np.asarray(x, dtype=float)
+    return 1/(1 + np.exp(-x))
+
+print(sigmoid([0,2,-2]))
+print(sigmoid([0]))
+print(sigmoid([[-1,0],[1,2]]))
