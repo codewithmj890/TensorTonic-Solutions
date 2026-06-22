ba9f98276ef864ac54a095140cd1176af8ca46a0
MRITYUNJAY SINGH <codewithmj890@gmail.com>
2026-06-22 15:43:41 +0530

    Solve kl-divergence

---
+++ kl-divergence/kl-divergence.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def kl_divergence(p, q, eps=1e-12):
+    p = np.asarray(p, dtype= np.float64)
+    q = np.asarray(q, dtype= np.float64)
+
+    mask = p > 0
+
+    return float(np.sum(p[mask] * np.log(p[mask] / (q[mask] + eps))))
