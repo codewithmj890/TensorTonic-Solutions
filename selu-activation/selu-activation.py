51b92e763f1ed981d99c562230a3056718ea9d69
MRITYUNJAY SINGH <codewithmj890@gmail.com>
2026-07-10 20:25:37 +0530

    Solve selu-activation

---
+++ selu-activation/selu-activation.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+def selu(x, lam=1.0507009873554804934193349852946, alpha=1.6732632423543772848170429916717):
+    x = np.asarray(x, dtype=np.float64)
+    result = np.where(x > 0, lam * x, lam * alpha * (np.exp(x) - 1))
+    return [round(float(v), 4) for v in result]
