b60e2112f1d097a1cc83c94d44b93d939d03393d
MRITYUNJAY SINGH <codewithmj890@gmail.com>
2026-07-11 17:46:02 +0530

    Solve discount-returns

---
+++ discount-returns/discount-returns.py
@@ -0,0 +1,7 @@
+def discount_returns(rewards, gamma):
+    T = len(rewards)
+    G = [0.0] * T
+    G[T - 1] = rewards[T - 1]
+    for t in range(T - 2, -1, -1):
+        G[t] = rewards[t] + gamma * G[t + 1]
+    return G
