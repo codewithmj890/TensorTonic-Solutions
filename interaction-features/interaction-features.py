165de3ce5b33fec45654422e3d75f34c9075acd1
MRITYUNJAY SINGH <codewithmj890@gmail.com>
2026-07-16 19:37:12 +0530

    Solve interaction-features

---
+++ interaction-features/interaction-features.py
@@ -0,0 +1,10 @@
+def interaction_features(X):
+    result = []
+    for row in X:
+        d = len(row)
+        interactions = []
+        for i in range(d):
+            for j in range(i + 1, d):
+                interactions.append(row[i] * row[j])
+        result.append(list(row) + interactions)
+    return result
