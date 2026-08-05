7a3220406757ec41fee1baf48c68fbfbe0b71385
MRITYUNJAY SINGH <codewithmj890@gmail.com>
2026-08-05 23:32:19 +0530

    Solve gradient-descent-quadratic

--- gradient-descent-quadratic/gradient-descent-quadratic.py
+++ gradient-descent-quadratic/gradient-descent-quadratic.py
@@ -1,6 +1,9 @@
 def gradient_descent_quadratic(a, b, c, x0, lr, steps):
+    """Return final x after 'steps' iterations."""
     x = x0
     for _ in range(steps):
-        grad = 2 * a * x + b      # f'(x) = 2ax + b
-        x = x - lr * grad         # x = x - lr * f'(x)
-    return float(x)
+        # Derivative of f(x) = a*x^2 + b*x + c is f'(x) = 2*a*x + b
+        grad = 2 * a * x + b
+        x = x - lr * grad
+    return x
+    
